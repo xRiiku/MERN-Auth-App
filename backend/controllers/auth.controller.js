44cf4cf581ab72a589cef3c1b2989c53c2592d9a
Daniel Ramon Campos <drc89.sxm@gmail.com>
2024-04-03 16:58:50 +0200

    cookie

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -26,13 +26,8 @@ export const signin = async (req, res, next) => {
         const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
         const { password: hashedPassword, ...rest } = validUser._doc;
         const expiryDate = new Date(Date.now() + (3600000 * 24)); // 24 hours
-        res.cookie('access_token', token, { 
-            httpOnly: true, 
-            expires: expiryDate,
-            domain: 'mernauthapp.rikudev.com',
-            secure: true,
-            sameSite: 'none'
-        })
+        res
+        .cookie('access_token', token, { httpOnly: true, expires: expiryDate })
         .status(200)
         .json(rest);
     } catch (error) {
@@ -47,13 +42,11 @@ export const google = async (req, res, next) => {
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
         const { password: hashedPassword, ...rest } = user._doc;
         const expiryDate = new Date(Date.now() + 3600000); // 1 hour
-        res.cookie('access_token', token, { 
-            httpOnly: true, 
+        res
+            .cookie('access_token', token, {
+            httpOnly: true,
             expires: expiryDate,
-            domain: 'mernauthapp.rikudev.com',
-            secure: true,
-            sameSite: 'none'
-        })
+            })
             .status(200)
             .json(rest);
         } else {
@@ -73,13 +66,11 @@ export const google = async (req, res, next) => {
         const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
         const { password: hashedPassword2, ...rest } = newUser._doc;
         const expiryDate = new Date(Date.now() + 3600000); // 1 hour
-        res.cookie('access_token', token, { 
-            httpOnly: true, 
+        res
+            .cookie('access_token', token, {
+            httpOnly: true,
             expires: expiryDate,
-            domain: 'mernauthapp.rikudev.com',
-            secure: true,
-            sameSite: 'none'
-        })
+            })
             .status(200)
             .json(rest);
         }
