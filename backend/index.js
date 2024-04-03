39e3e0501c1560eed8b35fcfb5ac9fa8de36083d
Daniel Ramon Campos <drc89.sxm@gmail.com>
2024-04-03 16:32:16 +0200

    asd

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,37 @@
 import express from 'express';
+import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
-import path from 'path';
 
 import userRoutes from './routes/user.route.js'
 import authRoutes  from './routes/auth.route.js'
 import db from './db/db.js'
 
-const __dirname = path.resolve();
+
 const app = express();
 dotenv.config();
 const PORT = 3001;
 const URL = process.env.URL || 'http://localhost';
+const allowedOrigins = ['http://localhost:5173', 'http://localhost:3001', 'https://mern-auth-app-sigma.vercel.app', 'https://mernauthapp.rikudev.com'];
 
 
 app.use(express.json());
 app.use(cookieParser())
+app.use(cors({
+    origin: function(origin, callback) {
+      // Permitir solicitudes si el origen está en la lista de orígenes permitidos o si no se proporciona origen (solicitud no CORS)
+        if (!origin || allowedOrigins.includes(origin) || origin.startsWith('https://mernauthapp.rikudev.com') || origin.startsWith('https://mern-auth-app-sigma.vercel.app')) {
+        callback(null, true);
+        } else {
+        callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true // Habilitar el envío de cookies u otras credenciales con la solicitud
+}));
 db.initDB();
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${URL}:${PORT}`);
-});
-
-app.use(express.static(path.join(__dirname, '/frontend/dist')));
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
 });
 
 app.use("/user", userRoutes)
