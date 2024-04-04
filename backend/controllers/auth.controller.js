0b21bf5234d7dae1363d4dbcf474f4b23a1ed083
Daniel Ramon Campos <drc89.sxm@gmail.com>
2024-04-04 09:41:09 +0200

    none

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -27,7 +27,7 @@ export const signin = async (req, res, next) => {
         const { password: hashedPassword, ...rest } = validUser._doc;
         const expiryDate = new Date(Date.now() + (3600000 * 24)); // 24 hours
         res
-        .cookie('access_token', token, { httpOnly: true, expires: expiryDate, secure: true, sameSite: 'Strict', partitioned: true})
+        .cookie('access_token', token, { httpOnly: true, expires: expiryDate, secure: true, sameSite: 'none', partitioned: true})
         .status(200)
         .json(rest);
     } catch (error) {
@@ -47,7 +47,7 @@ export const google = async (req, res, next) => {
             httpOnly: true,
             expires: expiryDate,
             secure: true, 
-            sameSite: 'Strict',
+            sameSite: 'none',
             partitioned: true
             })
             .status(200)
@@ -74,7 +74,7 @@ export const google = async (req, res, next) => {
             httpOnly: true,
             expires: expiryDate,
             secure: true, 
-            sameSite: 'Strict', 
+            sameSite: 'none', 
             partitioned: true
             })
             .status(200)
